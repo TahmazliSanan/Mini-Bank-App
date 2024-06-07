b144dfeb6d96cbc224affcbb3277c4620f512b84
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-07 22:18:58 +0400

    Creation of project

---
+++ src/main/java/org/pronet/app/FlexBankApplication.java
@@ -0,0 +1,13 @@
+package org.pronet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pronet/app/FlexBankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pronet.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
