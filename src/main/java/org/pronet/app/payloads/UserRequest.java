921e8b8820d25b940c38331babaae5f3f060fbd8
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-07 22:43:48 +0400

    Creation of "UserRequest" payload

---
+++ src/main/java/org/pronet/app/payloads/UserRequest.java
@@ -0,0 +1,19 @@
+package org.pronet.app.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+    private String address;
+}
