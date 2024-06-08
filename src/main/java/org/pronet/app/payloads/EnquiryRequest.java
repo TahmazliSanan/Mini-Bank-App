9acb849b2cff854d5479fbcf6953afe4f95e0fe3
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-08 12:59:27 +0400

    Creation of "EnquiryRequest" payload

---
+++ src/main/java/org/pronet/app/payloads/EnquiryRequest.java
@@ -0,0 +1,14 @@
+package org.pronet.app.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnquiryRequest {
+    private String accountNumber;
+}
