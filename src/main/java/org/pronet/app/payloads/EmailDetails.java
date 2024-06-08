56546551be5271defa1190df9d312562868f2741
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-08 11:40:25 +0400

    Creation of "EmailDetails" payload

---
+++ src/main/java/org/pronet/app/payloads/EmailDetails.java
@@ -0,0 +1,16 @@
+package org.pronet.app.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDetails {
+    private String recipient;
+    private String subject;
+    private String textBody;
+}
