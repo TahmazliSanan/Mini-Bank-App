c3d5d3ca33a35da70c01c11869722154c48e95b1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-09 20:47:30 +0400

    Add new functionalities

--- src/main/java/org/pronet/app/services/EmailService.java
+++ src/main/java/org/pronet/app/services/EmailService.java
@@ -3,5 +3,6 @@ package org.pronet.app.services;
 import org.pronet.app.payloads.EmailDetails;
 
 public interface EmailService {
-    void sendEmail(EmailDetails emailDetails);
+    void sendEmail(EmailDetails details);
+    void sendEmailWithAttachment(EmailDetails details);
 }
--- src/main/java/org/pronet/app/services/implementations/EmailServiceImpl.java
+++ src/main/java/org/pronet/app/services/implementations/EmailServiceImpl.java
@@ -1,34 +1,61 @@
 package org.pronet.app.services.implementations;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.pronet.app.payloads.EmailDetails;
 import org.pronet.app.services.EmailService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Objects;
 
 @Service
 public class EmailServiceImpl implements EmailService {
     @Value("${spring.mail.username}")
     private String senderMail;
-    private final JavaMailSender jms;
+    private final JavaMailSender mailSender;
 
-    public EmailServiceImpl(JavaMailSender jms) {
-        this.jms = jms;
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
     @Override
-    public void sendEmail(EmailDetails emailDetails) {
+    public void sendEmail(EmailDetails details) {
         try {
-            SimpleMailMessage smm = new SimpleMailMessage();
-            smm.setFrom(senderMail);
-            smm.setTo(emailDetails.getRecipient());
-            smm.setSubject(emailDetails.getSubject());
-            smm.setText(emailDetails.getTextBody());
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderMail);
+            mailMessage.setTo(details.getRecipient());
+            mailMessage.setSubject(details.getSubject());
+            mailMessage.setText(details.getTextBody());
 
-            jms.send(smm);
+            mailSender.send(mailMessage);
         } catch (MailException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void sendEmailWithAttachment(EmailDetails details) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper;
+        try {
+            messageHelper = new MimeMessageHelper(mimeMessage, true);
+            messageHelper.setFrom(senderMail);
+            messageHelper.setTo(details.getRecipient());
+            messageHelper.setSubject(details.getSubject());
+            messageHelper.setText(details.getTextBody());
+
+            FileSystemResource file = new FileSystemResource(new File(details.getAttachment()));
+            messageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+
+            mailSender.send(mimeMessage);
+        } catch (MessagingException ex) {
             throw new RuntimeException(ex);
         }
     }
