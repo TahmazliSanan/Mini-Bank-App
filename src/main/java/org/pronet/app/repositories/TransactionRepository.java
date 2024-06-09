c07ffecd822152766d63c6625c2320631db0f669
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-09 12:03:06 +0400

    Creation of TransactionRepository

---
+++ src/main/java/org/pronet/app/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package org.pronet.app.repositories;
+
+import org.pronet.app.entities.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
