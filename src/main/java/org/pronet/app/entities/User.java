c6f3de72ea14930a322e8e94c5082750ff597a4e
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-10 13:42:11 +0400

    Add new field

--- src/main/java/org/pronet/app/entities/User.java
+++ src/main/java/org/pronet/app/entities/User.java
@@ -22,6 +22,7 @@ public class User {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
     private String phoneNumber;
     private String gender;
     private String address;
--- src/main/java/org/pronet/app/payloads/UserRequest.java
+++ src/main/java/org/pronet/app/payloads/UserRequest.java
@@ -13,6 +13,7 @@ public class UserRequest {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
     private String phoneNumber;
     private String gender;
     private String address;
