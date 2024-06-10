ba7b78002d5de0b927964a5d5612e3e068c98d1c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-06-10 20:24:01 +0400

    Creation of CustomUserDetailsServiceImpl

---
+++ src/main/java/org/pronet/app/services/implementations/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package org.pronet.app.services.implementations;
+
+import org.pronet.app.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " is not found!"));
+    }
+}
